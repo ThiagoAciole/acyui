c79e1c20fd5dbc06ddc3d655febe8c1f0947e61f
ThiagoAciole <thiagoaciole7@gmail.com>
2026-03-12 18:28:35 -0300

    feat: Add main entry point for the playground application with React and ThemeProvider.

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './index.css'
-import { ThemeProvider } from 'acyon'
+import { ThemeProvider } from '@aciole/acyon'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
